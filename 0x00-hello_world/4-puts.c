4250bf5e1346d18fd58308138a0e20b537e6b369
DawitLemmaBalcha <dawit.lemma.balcha@gmail.com>
2023-02-07 15:15:34 +0300

    printing with puts -minor fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main(void)
+/*
+  main -Entry point
+*/
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
